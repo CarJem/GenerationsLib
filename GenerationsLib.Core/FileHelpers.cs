9b814a6bc59ab8375b4988d24b5eea9013db0390
CarJem Generations <Cwallacecs@Gmail.com>
2019-11-26 13:51:43 -0500

    New Classes Added

---
+++ GenerationsLib.Core/FileHelpers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace GenerationsLib.Core
+{
+    public static class FileHelpers
+    {
+        public static bool IsFileLocked(FileInfo file)
+        {
+            try
+            {
+                using (FileStream stream = file.Open(FileMode.Open, FileAccess.Read, FileShare.None))
+                {
+                    stream.Close();
+                }
+            }
+            catch (IOException)
+            {
+                //the file is unavailable because it is:
+                //still being written to
+                //or being processed by another thread
+                //or does not exist (has already been processed)
+                return true;
+            }
+
+            //file is not locked
+            return false;
+        }
+    }
+}
