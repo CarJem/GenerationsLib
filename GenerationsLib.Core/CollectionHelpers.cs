93bbe9404c1301879b0ff6553efc2f2493234eca
CarJem Generations <Cwallacecs@Gmail.com>
2019-12-03 11:34:25 -0500

    Added a Collection Helper (KeyPairList to Dict)

---
+++ GenerationsLib.Core/CollectionHelpers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerationsLib.Core
+{
+    public static class CollectionHelpers
+    {
+        public static Dictionary<TKey, TElement> ToDictionary<TSource, TKey, TElement>(IEnumerable<TSource> source, Func<TSource, TKey> keySelector, Func<TSource, TElement> elementSelector)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+            if (keySelector == null)
+            {
+                throw new ArgumentNullException("keySelector");
+            }
+            if (elementSelector == null)
+            {
+                throw new ArgumentNullException("elementSelector");
+            }
+
+            var dictionary = new Dictionary<TKey, TElement>();
+            foreach (TSource current in source)
+            {
+                dictionary.Add(keySelector(current), elementSelector(current));
+            }
+            return dictionary;
+        }
+    }
+}
