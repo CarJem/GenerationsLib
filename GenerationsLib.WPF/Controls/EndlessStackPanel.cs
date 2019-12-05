4077c6410c9a97ea7e85deef086507d1043adbc3
CarJem Generations <Cwallacecs@Gmail.com>
2019-12-05 16:49:17 -0500

    Updates to the Theming Implementations (includes RSDKrU Legacy Elements)

--- GenerationsLib.WPF/Controls/EndlessStackPanel.cs
+++ GenerationsLib.WPF/Controls/EndlessStackPanel.cs
@@ -13,7 +13,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Drawing;
 using System.IO;
 using System.Windows.Converters;
 using System.Windows.Diagnostics;
