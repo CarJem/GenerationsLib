372813c656e06deadc7fdb382562a7fa89baee0a
CarJem Generations <Cwallacecs@Gmail.com>
2019-12-07 14:26:42 -0500

    File Helper Additions

--- GenerationsLib.Core/FileHelpers.cs
+++ GenerationsLib.Core/FileHelpers.cs
@@ -30,5 +30,109 @@ namespace GenerationsLib.Core
             //file is not locked
             return false;
         }
+
+        public static void CopyDirectory(DirectoryInfo source, DirectoryInfo destination)
+        {
+            if (!destination.Exists)
+            {
+                destination.Create();
+            }
+
+            // Copy all files.
+            FileInfo[] files = source.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                file.CopyTo(Path.Combine(destination.FullName,
+                    file.Name));
+            }
+
+            // Process subdirectories.
+            DirectoryInfo[] dirs = source.GetDirectories();
+            foreach (DirectoryInfo dir in dirs)
+            {
+                // Get destination directory.
+                string destinationDir = Path.Combine(destination.FullName, dir.Name);
+
+                // Call CopyDirectory() recursively.
+                CopyDirectory(dir, new DirectoryInfo(destinationDir));
+            }
+        }
+
+        public static void DeleteFilesFiltered(DirectoryInfo source, List<string> folderNamesToIgnore = null)
+        {
+            foreach (DirectoryInfo dir in source.GetDirectories())
+            {
+                if (!(folderNamesToIgnore != null && folderNamesToIgnore.Contains(dir.Name)))
+                {
+                    dir.Delete(true);
+                }
+            }
+
+            foreach (FileInfo file in source.GetFiles())
+            {
+                file.Delete();
+            }
+
+        }
+
+        public static void MoveFilesRecursively(DirectoryInfo source, DirectoryInfo target, List<string> folderNamesToIgnore = null)
+        {
+            foreach (DirectoryInfo dir in source.GetDirectories())
+            {
+                if (!(folderNamesToIgnore != null && folderNamesToIgnore.Contains(dir.Name)))
+                {
+                    MoveFilesRecursivelyInternal(dir, target.CreateSubdirectory(dir.Name));
+                }
+            }
+
+            foreach (FileInfo file in source.GetFiles())
+            {
+                file.MoveTo(Path.Combine(target.FullName, file.Name));
+            }
+
+        }
+
+        private static void MoveFilesRecursivelyInternal(DirectoryInfo source, DirectoryInfo target)
+        {
+            foreach (DirectoryInfo dir in source.GetDirectories())
+            {
+                MoveFilesRecursivelyInternal(dir, target.CreateSubdirectory(dir.Name));
+            }
+
+            foreach (FileInfo file in source.GetFiles())
+            {
+                file.MoveTo(Path.Combine(target.FullName, file.Name));
+            }
+        }
+
+        public static void CopyFilesRecursively(DirectoryInfo source, DirectoryInfo target, List<string> folderNamesToIgnore = null)
+        {
+            foreach (DirectoryInfo dir in source.GetDirectories())
+            {
+                if (!(folderNamesToIgnore != null && folderNamesToIgnore.Contains(dir.Name)))
+                {
+                    CopyFilesRecursivelyInternal(dir, target.CreateSubdirectory(dir.Name));
+                }
+            }
+
+            foreach (FileInfo file in source.GetFiles())
+            {
+                file.CopyTo(Path.Combine(target.FullName, file.Name));
+            }
+
+        }
+
+        private static void CopyFilesRecursivelyInternal(DirectoryInfo source, DirectoryInfo target)
+        {
+            foreach (DirectoryInfo dir in source.GetDirectories())
+            {
+                CopyFilesRecursivelyInternal(dir, target.CreateSubdirectory(dir.Name));
+            }
+
+            foreach (FileInfo file in source.GetFiles())
+            {
+                file.CopyTo(Path.Combine(target.FullName, file.Name));
+            }
+        }
     }
 }
