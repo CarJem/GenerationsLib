c73c87f8690c06080e2eda4a4b79616c82e66b07
CarJem Generations <Cwallacecs@Gmail.com>
2020-03-02 18:41:07 -0500

    List Helper Class Additions

---
+++ GenerationsLib.Core/EventList.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerationsLib.Core
+{
+    public class EventList<T> : IList<T>
+    {
+        private readonly List<T> _list;
+
+        public EventList()
+        {
+            _list = new List<T>();
+        }
+
+        public EventList(IEnumerable<T> collection)
+        {
+            _list = new List<T>(collection);
+        }
+
+        public EventList(int capacity)
+        {
+            _list = new List<T>(capacity);
+        }
+
+        public event EventHandler<EventListArgs<T>> ItemAdded;
+        public event EventHandler<EventListArgs<T>> ItemRemoved;
+
+        private void RaiseEvent(EventHandler<EventListArgs<T>> eventHandler, T item, int index)
+        {
+            var eh = eventHandler;
+            eh?.Invoke(this, new EventListArgs<T>(item, index));
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(T item)
+        {
+            var index = _list.Count;
+            _list.Add(item);
+            RaiseEvent(ItemAdded, item, index);
+        }
+
+        public void Clear()
+        {
+            for (var index = 0; index < _list.Count; index++)
+            {
+                var item = _list[index];
+                RaiseEvent(ItemRemoved, item, index);
+            }
+
+            _list.Clear();
+        }
+
+        public bool Contains(T item)
+        {
+            return _list.Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            _list.CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(T item)
+        {
+            var index = _list.IndexOf(item);
+
+            if (_list.Remove(item))
+            {
+                RaiseEvent(ItemRemoved, item, index);
+                return true;
+            }
+
+            return false;
+        }
+
+        public int Count => _list.Count;
+        public bool IsReadOnly => false;
+
+        public int IndexOf(T item)
+        {
+            return _list.IndexOf(item);
+        }
+
+        public void Insert(int index, T item)
+        {
+            _list.Insert(index, item);
+            RaiseEvent(ItemAdded, item, index);
+        }
+
+        public bool Exists(Predicate<T> match)
+        {
+            return _list.Exists(match);
+        }
+
+        public void RemoveAt(int index)
+        {
+            var item = _list[index];
+            _list.RemoveAt(index);
+            RaiseEvent(ItemRemoved, item, index);
+        }
+
+        public T this[int index]
+        {
+            get { return _list[index]; }
+            set { _list[index] = value; }
+        }
+    }
+
+    public class EventListArgs<T> : EventArgs
+    {
+        public EventListArgs(T item, int index)
+        {
+            Item = item;
+            Index = index;
+        }
+
+        public T Item { get; }
+        public int Index { get; }
+    }
+}
--- GenerationsLib.Core/ListHelpers.cs
+++ GenerationsLib.Core/ListHelpers.cs
@@ -8,6 +8,38 @@ namespace GenerationsLib.Core
 {
     public static class ListHelpers
     {
+        /// <summary>
+        ///   Checks whether all items in the enumerable are same (Uses <see cref="object.Equals(object)" /> to check for equality)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="enumerable">The enumerable.</param>
+        /// <returns>
+        ///   Returns true if there is 0 or 1 item in the enumerable or if all items in the enumerable are same (equal to
+        ///   each other) otherwise false.
+        /// </returns>
+        public static bool AreAllSame<T>(this IEnumerable<T> enumerable)
+        {
+            if (enumerable == null) throw new ArgumentNullException(nameof(enumerable));
+
+            using (var enumerator = enumerable.GetEnumerator())
+            {
+                var toCompare = default(T);
+                if (enumerator.MoveNext())
+                {
+                    toCompare = enumerator.Current;
+                }
+
+                while (enumerator.MoveNext())
+                {
+                    if (toCompare != null && !toCompare.Equals(enumerator.Current))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
         public static int MoveSelectedIndex(int listCount, int oldIndex, int newIndex)
         {
             if ((oldIndex == newIndex) || (0 > oldIndex) || (oldIndex >= listCount) || (0 > newIndex) || (newIndex >= listCount)) return oldIndex;
