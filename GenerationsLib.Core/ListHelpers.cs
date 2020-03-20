d0d89fd8c672e759ebfc2f6b5767da629a8f978a
CarJem Generations <Cwallacecs@Gmail.com>
2020-03-19 22:12:12 -0400

    Random Commit for Myself

--- GenerationsLib.Core/ListHelpers.cs
+++ GenerationsLib.Core/ListHelpers.cs
@@ -47,31 +47,9 @@ namespace GenerationsLib.Core
         }
         public static void Move<T>(this List<T> list, int oldIndex, int newIndex)
         {
-
-            // exit if possitions are equal or outside array
-            if ((oldIndex == newIndex) || (0 > oldIndex) || (oldIndex >= list.Count) || (0 > newIndex) ||
-                (newIndex >= list.Count)) return;
-            // local variables
-            var i = 0;
-            T tmp = list[oldIndex];
-            // move element down and shift other elements up
-            if (oldIndex < newIndex)
-            {
-                for (i = oldIndex; i < newIndex; i++)
-                {
-                    list[i] = list[i + 1];
-                }
-            }
-            // move element up and shift other elements down
-            else
-            {
-                for (i = oldIndex; i > newIndex; i--)
-                {
-                    list[i] = list[i - 1];
-                }
-            }
-            // put element from position 1 to destination
-            list[newIndex] = tmp;
+            T aux = list[newIndex];
+            list[newIndex] = list[oldIndex];
+            list[oldIndex] = aux;
         }
         public static TValue GetSafe<TItem, TValue>(this IList<TItem> list, int index, Func<TItem, TValue> selector, TValue defaultValue)
         {
