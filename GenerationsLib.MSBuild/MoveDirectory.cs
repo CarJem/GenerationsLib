7965db0700a1c841b83d63cb676a6955e4eae668
CarJem Generations <Cwallacecs@Gmail.com>
2023-08-02 14:24:39 -0700

    Improved MSBuild Logic

--- GenerationsLib.MSBuild/MoveDirectory.cs
+++ GenerationsLib.MSBuild/MoveDirectory.cs
@@ -2,11 +2,36 @@ using Microsoft.Build.Framework;
 using MSBuildTask = Microsoft.Build.Utilities.Task;
 using System.IO;
 using System;
+using System.Linq;
 
 namespace GenerationsLib.MSBuild
 {
     public class MoveDirectory : MSBuildTask
     {
+        public static void MoveDirectoryFunc(string source, string target)
+        {
+            var sourcePath = source.TrimEnd('\\', ' ');
+            var targetPath = target.TrimEnd('\\', ' ');
+
+            if (Directory.Exists(sourcePath))
+            {
+                var files = Directory.EnumerateFiles(sourcePath, "*", SearchOption.AllDirectories)
+                                     .GroupBy(s => Path.GetDirectoryName(s));
+                foreach (var folder in files)
+                {
+                    var targetFolder = folder.Key.Replace(sourcePath, targetPath);
+                    Directory.CreateDirectory(targetFolder);
+                    foreach (var file in folder)
+                    {
+                        var targetFile = Path.Combine(targetFolder, Path.GetFileName(file));
+                        if (File.Exists(targetFile)) File.Delete(targetFile);
+                        File.Move(file, targetFile);
+                    }
+                }
+                Directory.Delete(source, true);
+            }
+
+        }
         public override bool Execute()
         {
             try
@@ -15,7 +40,8 @@ namespace GenerationsLib.MSBuild
                 var Destination = Path.Combine(DestinationFolder.ToString(), new DirectoryInfo(Source).Name);
                 try
                 {
-                    Directory.Move(Source, Destination);
+
+                    MoveDirectoryFunc(Source, Destination);
                 }
                 catch (Exception ex2)
                 {
